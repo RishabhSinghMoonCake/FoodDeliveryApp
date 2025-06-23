45b327558e0b2a4ec3930214b096cecdd6dcdff0
RishabhSinghMoonCake <rishabhsinghmooncake@gmail.com>
2025-06-23 19:02:26 +0530

    adding navbar and sidebar to admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,24 @@
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/sidebar/Sidebar"
+import Add from "./pages/Add/Add"
+
+import {Routes,Route} from 'react-router'
+import List from "./pages/List/List"
+import Orders from "./pages/Orders/Orders"
+
 function App() {
   return(
     <div>
-
+      <Navbar/>
+      <hr />
+      <div className="app-content">
+        <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>} />
+          <Route path='/list' element={<List/>} />
+          <Route path='/orders' element={<Orders/>} />
+        </Routes>
+      </div>
     </div>
   )
 }
