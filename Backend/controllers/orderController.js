f3c813c174f6bb540dc1b051edeff204571d1de9
RishabhSinghMoonCake <rishabhsinghmooncake@gmail.com>
2025-06-28 15:17:29 +0530

    Update orderController.js

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 //placing user order from frontend
 
 const placeOrder = async (req,res) => {
-  const frontendUrl = 'https://khaanpann-frontend.onrender.com/'
+  const frontendUrl = 'https://khaanpann-frontend.onrender.com'
   try {
     const newOrder = new orderModel({
       userId:req.body.userId,
